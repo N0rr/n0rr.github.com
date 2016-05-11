41223c9570b5501981ad7b82c580ff2be4cc831e
N0rr <xt12pabeh@mail.ru>
2016-05-11 20:18:35 +0300

    f

--- form/mail.php
+++ form/mail.php
@@ -9,5 +9,4 @@ $text = trim($_POST["text"]);
 $message = "Имя: $name \nТелефон: $phone \nТекст: $text";
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"UTF-8\"\n From: $recepient"); 
-?>
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
