9630a26a9a54dff1200594e316093836707d3e13
N0rr <xt12pabeh@mail.ru>
2016-05-11 14:30:46 +0300

    2

--- wantRock/form/mail.php
+++ wantRock/form/mail.php
@@ -10,4 +10,3 @@ $message = "Имя: $name \nТелефон: $phone \nТекст: $text";
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient"); 
-?>
